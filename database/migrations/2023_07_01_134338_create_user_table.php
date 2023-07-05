999ebd8c8f67e83e25060369bc39def5f7fc0a58
vinanamira <mzssdaru@gmail.com>
2023-07-05 21:08:53 +0700

    yyy

--- database/migrations/2023_07_01_134338_create_user_table.php
+++ database/migrations/2023_07_01_134338_create_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('file', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('file');
+        Schema::dropIfExists('user');
     }
 };
